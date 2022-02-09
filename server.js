2671736c407f3fa18d5e211bdd12d1b354efc7c8
yl9517 <yl9517@naver.com>
2022-02-09 15:31:18 +0900

    test gitaction

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const express = require("express");
 const PORT = 3000;
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello World! It's version2 "));
 app.listen(PORT, console.log(`Server listening on port ${PORT}`)); // listen : 해당 port번호를 듣고있다가, request(요청)이 들어오면 작동
