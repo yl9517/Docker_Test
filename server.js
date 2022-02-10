293151a27f28fab48b549078c8a701593650bafb
yl9517 <yl9517@naver.com>
2022-02-10 20:45:03 +0900

    fix port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
-const PORT = 3000;
+const PORT = 8080;
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World! It's version3 "));
+app.get("/", (req, res) =>
+  res.send(`Hello World! It's version5 port :${PORT}`)
+);
 app.listen(PORT, console.log(`Server listening on port ${PORT}`)); // listen : 해당 port번호를 듣고있다가, request(요청)이 들어오면 작동
