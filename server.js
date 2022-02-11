7b058fd22ced14e8ea70a1cbbdd1965fa7982bc9
yl9517 <yl9517@naver.com>
2022-02-11 10:13:25 +0900

    one more test

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const PORT = 8080;
 const app = express();
 
 app.get("/", (req, res) =>
-  res.send(`Hello World! It's version5 port :${PORT}`)
+  res.send(`Hello World! It's version6 port :${PORT}`)
 );
 app.listen(PORT, console.log(`Server listening on port ${PORT}`)); // listen : 해당 port번호를 듣고있다가, request(요청)이 들어오면 작동
